67c98c891e9e442ab6322925a0ff87c94c5a49f3
Krishsharma3 <147696378+Krishsharma3@users.noreply.github.com>
2024-12-06 19:42:55 +0530

    Update sure.py

--- sure.py
+++ sure.py
@@ -2,3 +2,6 @@ print("I've drink today!!")
 Print("I'll definatly drink today")
 print("No One can stop me to drink today")
 print("Literly telling myself i'll drink there is no way")
+
+
+print("changes need to done")
